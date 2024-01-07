29b43141639a390dbc5bbec54cc7092d750b5e7e
kalebbroo <kalebbroo@gmail.com>
2024-01-07 15:58:25 -0500

    Added more buttons and placed them into a grid.

--- testmauiproject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/testmauiproject.AssemblyInfo.cs
+++ testmauiproject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/testmauiproject.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+93cc820bad1a1c76746db858fd0dc62704a2500a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5da1c721c3622949f9407fe99a5e70d3117b5e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
