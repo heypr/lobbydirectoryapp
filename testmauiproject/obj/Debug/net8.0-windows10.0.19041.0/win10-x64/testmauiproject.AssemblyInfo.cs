bfec583dd5b7f9c06eb8179e5b86abdd20c3cc51
kalebbroo <kalebbroo@gmail.com>
2024-01-07 21:40:03 -0500

    added 2 new pages

--- testmauiproject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/testmauiproject.AssemblyInfo.cs
+++ testmauiproject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/testmauiproject.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+29b43141639a390dbc5bbec54cc7092d750b5e7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e4ac384aaccb69aea8ec1287c577d389cf993f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testmauiproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
